41180249c29021f80d2de04d70b4c939d553ec04
GSATLWF <54094752+YytRecg@users.noreply.github.com>
2023-02-22 22:40:17 -0500

    minor modification

--- app/src/main/java/com/example/myapplication2/LoginActivity.java
+++ app/src/main/java/com/example/myapplication2/LoginActivity.java
@@ -5,9 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
