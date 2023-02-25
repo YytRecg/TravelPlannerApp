0479c4919ba524706c3109bc6b7248bddcefaa3b
GSATLWF <54094752+YytRecg@users.noreply.github.com>
2023-02-24 19:16:24 -0500

    Add lifecycle methods for TestActivity and DashboardFragment

--- app/src/main/java/com/example/myapplication2/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/myapplication2/ui/dashboard/DashboardFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+//import android.content.Context;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -16,6 +17,7 @@ import com.example.myapplication2.databinding.FragmentDashboardBinding;
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
+    private static final String FRAGMENT_NAME = DashboardFragment.class.getSimpleName();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -35,6 +37,56 @@ public class DashboardFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        Log.i("D_LIFECYCLE", FRAGMENT_NAME +" onDestroyView");
         binding = null;
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i("D_LIFECYCLE", FRAGMENT_NAME +" onCreate");
+    }
+
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        Log.i("D_LIFECYCLE", FRAGMENT_NAME +" onAttach");
+//    }
+
+    @Override
+    public void onStart() {
+        Log.i("D_LIFECYCLE", FRAGMENT_NAME +" onStart");
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        Log.i("D_LIFECYCLE", FRAGMENT_NAME +" onResume");
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        Log.i("D_LIFECYCLE", FRAGMENT_NAME +" onPause");
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Log.i("D_LIFECYCLE", FRAGMENT_NAME +" onStop");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i("D_LIFECYCLE", FRAGMENT_NAME +" onDestroy");
+        super.onDestroy();
+    }
+
+//    @Override
+//    public void onDetach() {
+//        Log.i("D_LIFECYCLE", FRAGMENT_NAME +" onDetach");
+//        super.onDetach();
+//    }
+
 }
