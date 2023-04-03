851c650bff420d693f2111f3a68417efa0b37b6d
GSATLWF <54094752+YytRecg@users.noreply.github.com>
2023-04-03 10:05:53 -0400

    added Image generator

--- app/src/main/java/com/example/myapplication2/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myapplication2/ui/home/HomeFragment.java
@@ -1,28 +1,43 @@
 package com.example.myapplication2.ui.home;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.bumptech.glide.Glide;
+import com.example.myapplication2.FlickrAPI;
+import com.example.myapplication2.LMPhoto;
+import com.example.myapplication2.R;
 import com.example.myapplication2.databinding.FragmentHomeBinding;
 
 import com.google.firebase.database.*;
-import com.google.android.gms.tasks.*;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.*;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    ImageView lmImageView;
+    public static String userDestInput;
+
+    List<LMPhoto> lmPhotoList;
+    int currPhotoIdx = -1;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -35,10 +50,75 @@ public class HomeFragment extends Fragment {
         View root = binding.getRoot();
 
         final Button searchButton = binding.searchButton;
+        final Button nextImgButton = binding.nextImgButton;
         final EditText userDestination = binding.editTextDestination;
+        this.userDestInput = userDestination.getText().toString();
+
+        lmImageView = binding.homeImageView;
+        FlickrAPI flickrAPI = new FlickrAPI(this);
+        flickrAPI.fetchLMImages();
+
+//        searchButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                FlickrAPI flickrAPI = new FlickrAPI(this);
+//                flickrAPI.fetchLMImages();
+//                nextImgButton.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        nextPhoto();
+//                    }
+//                });
+//            }
+//        });
+//
+//        searchButton.setOnClickListener((View v) -> {
+//            FlickrAPI flickrAPI = new FlickrAPI(this);
+//            flickrAPI.fetchLMImages();
+//        });
+
+        nextImgButton.setOnClickListener((View v) -> {
+            nextPhoto();
+        });
 
         return root;
     }
+
+
+    public void receivedLMPhotos(List<LMPhoto> lmPhotoList) {
+        this.lmPhotoList = lmPhotoList;
+        nextPhoto();
+    }
+
+    private void nextPhoto() {
+        if (lmPhotoList != null && lmPhotoList.size() > 0){
+            currPhotoIdx++;
+            currPhotoIdx %= lmPhotoList.size();
+
+            LMPhoto lmPhoto = lmPhotoList.get(currPhotoIdx);
+
+//            Log.d("flickr", "current lmPhoto :"+lmPhoto.toString());
+            String imageUrl = lmPhoto.getPhotoURL();
+
+            Glide.with(this)
+                    .load(imageUrl)
+                    .centerCrop()
+                    .into(lmImageView);
+//            FlickrAPI flickrAPI = new FlickrAPI(this);
+//            flickrAPI.fetchPhotoBitmap(lmPhoto.getPhotoURL());
+        }
+    }
+
+//    private void openUnsplashPage(){
+//        Intent i = new Intent(getView().getContext(), UnsplashAuthActivity.class);
+//        startActivity(i);
+//    }
+
+
+//    public void receivedPhotoBitmap(Bitmap bitmap) {
+//        ImageView lmImageView = binding.homeImageView;
+//        lmImageView.setImageBitmap(bitmap);
+//    }
 
     private void writeNewPost(DatabaseReference myRef, String userBio, String username, String photo) {
 
